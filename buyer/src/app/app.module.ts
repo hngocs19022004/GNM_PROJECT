9fd665c8f882d440ce6e36410459812f36f9151b
nhlinhng267@gmail.com <nhlinhng267@gmail.com>
2025-03-09 23:06:30 +0700

    homepageFE

--- buyer/src/app/app.module.ts
+++ buyer/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
