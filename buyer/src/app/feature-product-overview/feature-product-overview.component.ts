52c913015b71b49264c9bda7f66db26e09b24bec
nhlinhng267@gmail.com <nhlinhng267@gmail.com>
2025-03-10 14:18:35 +0700

    productovwFE

---
+++ buyer/src/app/feature-product-overview/feature-product-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-product-overview',
+  standalone: false,
+  templateUrl: './feature-product-overview.component.html',
+  styleUrl: './feature-product-overview.component.css'
+})
+export class FeatureProductOverviewComponent {
+
+}
